a4ef2a8ff523cde8dc2f43fae399301a802705a6
niye96 <526864551@qq.com>
2018-04-11 22:07:58 +0800

    实现了文件上传系统

---
+++ filesystem/src/main/java/com/ices/filesystem/controller/FastDFSController.java
@@ -0,0 +1,28 @@
+package com.ices.filesystem.controller;
+
+import com.ices.filesystem.component.FastDFSClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author: ny
+ * @Date: Created in 20:31 2018/4/11 0011
+ */
+@RestController
+@CrossOrigin
+@RequestMapping(value = "file")
+public class FastDFSController {
+    @Autowired
+    FastDFSClient fastDFSClient;
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String upload(@RequestParam MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 省略业务逻辑代码。。。
+        String imgUrl = fastDFSClient.uploadFile(file);
+        return imgUrl;
+    }
+}
